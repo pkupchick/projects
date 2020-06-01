7f53397e92e77ece33e64a5ff915470012fa4921
Peter Kupchick <pkupchick@gmail.com>
2020-06-01 15:19:49 -0400

    Finished the project

--- W4D1/skeleton/lib/super_computer_player.rb
+++ W4D1/skeleton/lib/super_computer_player.rb
@@ -2,8 +2,11 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
-    new = TicTacToeNode
-
+    new = TicTacToeNode.new(game.board, mark)
+    kids = new.children
+    new = kids.select {|kid| return kid.prev_move_pos if kid.winning_node?(mark)}
+    new = kids.any? {|kid| return kid.prev_move_pos if !kid.losing_node?(mark)}
+    raise "HOLY SHEET YOU BEAT ME!"
   end
 end
 
