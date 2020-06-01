39fb02dd31054f8996399fddc8cc9083ae30dcc8
Peter Kupchick <pkupchick@gmail.com>
2020-06-01 10:07:43 -0400

    Second day of knights

---
+++ W4D1/knights_travails/knights.rb
@@ -0,0 +1,49 @@
+require_relative "00_tree_node"
+require "byebug"
+class KnightPathFinder
+    attr_reader :considered_positions
+
+    def initialize(initial_pos)
+        @start = initial_pos
+        @considered_positions = [initial_pos]
+        @root = PolyTreeNode.new(initial_pos)
+    end   
+    
+    def self.possible_moves(pos)
+        row, col = pos
+        [[row - 2,col - 1],[row - 2, col + 1],
+        [row - 1,col - 2],[row - 1, col + 2],
+        [row + 1,col - 2],[row + 1, col + 2],
+        [row + 2,col - 1],[row + 2, col + 1]]
+    end
+
+    def self.valid_moves(pos)
+        moves = self.possible_moves(pos)
+        moves.select do |move|
+            move.all? { |pair| pair > -1 && pair < 8 }
+        end
+    end    
+
+    def new_move_positions(pos)
+        moves = KnightPathFinder.valid_moves(pos)
+        new_pot_moves = moves.reject { |move| @considered_positions.include?(move) }
+        @considered_positions += new_pot_moves
+        new_pot_moves
+    end
+
+    def build_move_tree
+         queue = [@root]
+        until queue.empty?
+            step_1 = queue.shift
+            moves = new_move_positions(step_1.value)
+            moves.each do |move|
+                step_1.add_child(PolyTreeNode.new(move))
+            end
+            queue += step_1.children 
+        end       
+    end    
+end
+
+new = KnightPathFinder.new([0,0])
+
+p new.build_move_tree
