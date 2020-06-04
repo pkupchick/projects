a6ac6c92917bb25b85005ed8ef932913f9859b4b
Peter Kupchick <pkupchick@gmail.com>
2020-06-04 13:21:53 -0400

    Finished first part of TDD

---
+++ W4D4/tdd/lib/test_dd.rb
@@ -0,0 +1,6 @@
+
+def my_unique(arr)
+    arr.uniq
+end
+
+p my_unique([1,1,4,4,3])
--- W4D4/tdd/spec/test_dd_spec.rb
+++ W4D4/tdd/spec/test_dd_spec.rb
@@ -1,1 +1,15 @@
 require "test_dd"
+
+describe "#my_unique" do 
+    it "should accept an array" do 
+        expect {my_unique([1,1,2,3,3])}.to_not raise_error
+    end
+
+    it "should return an array" do 
+        expect(my_unique([1,1,2,3,3])).to eq([])
+    end
+
+    it "should return an array of only unique elements" do
+        expect(my_unique([1,1,2,3,3])).to eq([1,2,3])
+    end
+end
