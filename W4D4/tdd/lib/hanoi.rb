c1aa4a965ccc33525c6ada369974a0f8aeb9e822
Peter Kupchick <pkupchick@gmail.com>
2020-06-04 15:41:57 -0400

    Setting up poker with Nara

--- W4D4/tdd/lib/hanoi.rb
+++ W4D4/tdd/lib/hanoi.rb
@@ -1,7 +1,47 @@
 class Towers
+  attr_accessor :towers
+
     def initialize
-      @towers = [[3,2,1],[],[]]
+      @towers = [[3, 2, 1],[],[]]
     end
 
+    def move(grab, place)
+      if @towers[grab].empty?
+        raise "no discs in the stack"
+      elsif !valid_move?(grab,place)
+        raise "Cannot move larger disc onto smaller disc"
+      else
+        @towers[place] << @towers[grab].pop
+      end
+    end
     
+    def valid_move?(grab, place)
+      return true if @towers[place].empty?
+      if @towers[grab].last > @towers[place].last
+        false
+      else
+        true
+      end
+    end
+
+    def won?
+      return true if @towers == [[],[3,2,1],[]] || [[],[],[3,2,1]]
+    end
+
+
+    def make_move
+      begin
+        puts "enter a move example 1, 2"
+        input = gets.chomp.split(", ").map(&:to_i)
+        move(input[0], input[1])
+      rescue
+        retry
+      end
+    end
 end
+
+# new = Towers.new
+
+# new.move(0,2)
+# new.move(0,1)
+# p new.move(0,1)
--- W4D4/tdd/spec/hanoi_spec.rb
+++ W4D4/tdd/spec/hanoi_spec.rb
@@ -1,2 +1,55 @@
 require "hanoi"
 
+describe Towers do
+    subject(:towers) { Towers.new }
+
+    describe "#move" do
+        it "allows moving to open space" do
+            expect { towers.move(0, 1) }.not_to raise_error
+        end
+
+        it "does not allow moving discs from empty stack" do
+            expect { towers.move(2, 1) }.to raise_error("no discs in the stack")
+        end
+
+        it "allows moving onto a larger disc" do
+            towers.move(0, 2)
+            towers.move(0, 1)
+            expect { towers.move(2, 1) }.not_to raise_error
+        end
+
+        it "does not allow placing larger disc onto smaller disc" do
+            towers.move(0, 2)
+            expect { towers.move(0, 2) }.to raise_error("Cannot move larger disc onto smaller disc")
+        end
+    end
+
+    describe "#won?" do
+        it "game is won when all discs are moved to last tower" do
+            towers.move(0, 2)
+            towers.move(0, 1)
+            towers.move(2, 1)
+            towers.move(0, 2)
+            towers.move(1, 0)
+            towers.move(1, 2)
+            towers.move(0, 2)
+
+            expect(towers).to be_won
+        end
+
+        it "game is won when all discs are oved to middle tower" do
+            towers.move(0, 1)
+            towers.move(0, 2)
+            towers.move(1, 2)
+            towers.move(0, 1)
+            towers.move(2, 0)
+            towers.move(2 ,1)
+            towers.move(0, 1)
+
+            expect(towers).to be_won
+        end
+
+    end
+
+
+end
