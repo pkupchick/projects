7a05a2588b3af029663d7eea4ae3ff9b5fdcd50d
Peter Kupchick <pkupchick@gmail.com>
2020-06-09 15:20:05 -0400

    Finishing 03_nobel

--- W5D2/skeleton/lib/03_select_nobel.rb
+++ W5D2/skeleton/lib/03_select_nobel.rb
@@ -24,24 +24,49 @@ end
 def prizes_from_1950
   # Display Nobel prizes for 1950.
   execute(<<-SQL)
+    SELECT
+      *
+    FROM
+      nobels
+    WHERE
+      nobels.yr = 1950
+
   SQL
 end
 
 def literature_1962
   # Show who won the 1962 prize for Literature.
   execute(<<-SQL)
+  SELECT
+    nobels.winner
+  FROM
+    nobels
+  WHERE
+    nobels.subject = 'Literature' AND nobels.yr = 1962
   SQL
 end
 
 def einstein_prize
   # Show the year and subject that won 'Albert Einstein' his prize.
   execute(<<-SQL)
+    SELECT
+      nobels.yr, nobels.subject
+    FROM
+      nobels
+    WHERE
+      nobels.winner = 'Albert Einstein'
   SQL
 end
 
 def millennial_peace_prizes
   # Give the name of the 'Peace' winners since the year 2000, including 2000.
   execute(<<-SQL)
+  SELECT
+    nobels.winner
+  FROM
+    nobels
+  WHERE
+    nobels.yr >= 2000 AND nobels.subject = 'Peace'
   SQL
 end
 
