bb0a84cb7569f80fa61f1e1a4769fcf90982cc04
Peter Kupchick <pkupchick@gmail.com>
2020-06-11 16:08:24 -0400

    Anthony and I are kicking butt, finishing up ShortenedUrl model

---
+++ W5D4/URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,15 @@
+require 'securerandom'
+
+class ShortenedUrl < ApplicationRecord
+    validates :short_url, presence: true
+
+    def self.random_code
+        token = SecureRandom.urlsafe_base64
+        ShortenedUrl.exists?(token) ? ShortenedUrl.random_code : token 
+    end 
+
+    def create_short_url(user, long_url)
+        ShortenedUrl.create!(:short_url => ShortenedUrl.random_code)
+    end 
+
+end
--- W5D4/URLShortener/db/schema.rb
+++ W5D4/URLShortener/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_181718) do
+ActiveRecord::Schema.define(version: 2020_06_11_190341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shortened_urls", force: :cascade do |t|
+    t.string "long_url"
+    t.string "short_url"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_shortened_urls_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
